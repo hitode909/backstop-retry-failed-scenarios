06e46743271460b13cb3857c0d28dd5e87608f3e
hitode909 <hitode909@gmail.com>
2020-06-06 16:01:35 +0900

    writing CLI

--- cli.js
+++ cli.js
@@ -1,3 +1,52 @@
 #!/usr/bin/env node
 
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+
+const optionDefinitions = [
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'Display this usage guide.'
+  },
+  {
+    name: 'retry',
+    type: Number,
+    defaultValue: 3,
+    description: 'Retry count'
+  },
+  {
+    name: 'config',
+    type: String,
+    defaultValue: 'backstop.json',
+    description: 'Path to config file',
+  },
+  {
+    name: 'command',
+    type: String,
+    defaultValue: 'backstop test',
+    description: 'Command to run test',
+  },
+];
+const options = commandLineArgs(optionDefinitions);
+
+
+if (options.help) {
+  const usage = commandLineUsage([
+    {
+      header: 'backstop-retry-failed-scenarios',
+      content: 'A wrapper script to retry failed scenario for BackstopJS.'
+    },
+    {
+      header: 'Options',
+      optionList: optionDefinitions
+    },
+  ])
+  console.log(usage);
+  process.exit(0);
+}
+
+console.log(options);
+
 process.stdout.write("Hello, world!\n");
