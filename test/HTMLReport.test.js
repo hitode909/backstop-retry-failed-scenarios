4ff520988f3ad4443a40bfb6f81bff31aa1595de
hitode909 <hitode909@gmail.com>
2020-06-06 20:09:49 +0900

    write

--- test/HTMLReport.test.js
+++ test/HTMLReport.test.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const os = require('os');
 const { HTMLReport } = require('../lib/HTMLReport');
 
 const rootDir = process.cwd();
@@ -19,15 +20,20 @@ describe('HTMLReport', () => {
   });
 
   test('it merges test result', () => {
-    const r1 = new HTMLReport(path.join(process.cwd(), 'test', 'fixtures', 'backstop', 'failed', 'backstop_data', 'html_report', 'config.js'));
+    const r1 = new HTMLReport(path.join(process.cwd(), 'test', 'fixtures', 'backstop', 'tablet_success_sp_failed', 'backstop_data', 'html_report', 'config.js'));
     const r2 = new HTMLReport(path.join(process.cwd(), 'test', 'fixtures', 'backstop', 'pass', 'backstop_data', 'html_report', 'config.js'));
 
-    expect(r1.failedCount).toEqual(2);
+    expect(r1.failedCount).toEqual(1);
     expect(r2.failedCount).toEqual(0);
-    expect(r1.passCount).toEqual(0);
+    expect(r1.passCount).toEqual(1);
     expect(r2.passCount).toEqual(2);
     r1.notifyNewReport(r2);
     expect(r1.failedCount).toEqual(0);
     expect(r2.failedCount).toEqual(0);
+
+    const tmpPath = path.join(os.tmpdir(), Math.random().toString());
+    r1.writeTo(tmpPath);
+    const wroteReport = new HTMLReport(tmpPath);
+    expect(wroteReport.passCount).toEqual(2);
   });
 });
