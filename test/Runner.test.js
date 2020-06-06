2cd4d3f806ea5804f3bf04f68378d8570fe72fd5
hitode909 <hitode909@gmail.com>
2020-06-06 16:25:45 +0900

    writing runner

---
+++ test/Runner.test.js
@@ -0,0 +1,31 @@
+const { Runner } = require('../lib/Runner');
+
+describe('Runner', () => {
+  test('it receives options', () => {
+    const runner = new Runner({
+      retry: 3,
+      config: 'backstop.js',
+      command: 'backstop test',
+    });
+    expect(runner).toBeDefined();
+    expect(runner.retryCount).toEqual(3);
+    expect(runner.configPath).toEqual('backstop.js');
+    expect(runner.command).toEqual('backstop test');
+  });
+
+  test('it parses config', () => {
+    const runner = new Runner({
+      config: 'test/fixtures/backstop.config.json',
+    });
+    expect(runner.configFullPath).toBeDefined();
+    expect(runner.configObject).toBeDefined();
+    expect(runner.configObject.paths).toStrictEqual({
+        bitmaps_reference: "backstop_data/bitmaps_reference",
+        bitmaps_test: "backstop_data/bitmaps_test",
+        engine_scripts: "backstop_data/engine_scripts",
+        html_report: "backstop_data/html_report",
+        json_report: "backstop_data/json_report",
+        ci_report: "backstop_data/ci_report",
+    });
+  });
+});
