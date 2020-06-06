5308f04752d603a0dd04576647c48e45ec9401e9
hitode909 <hitode909@gmail.com>
2020-06-07 00:09:55 +0900

    jsonReportPath, ciReportPath

--- test/Config.test.js
+++ test/Config.test.js
@@ -14,11 +14,15 @@ describe('Config', () => {
     expect(config.rawConfig).toBeDefined();
     expect(config.htmlReportPath).toEqual(path.join(dir, 'backstop_data', 'html_report', 'config.js'));
     expect(config.htmlReport).toBeDefined();
+    expect(config.jsonReportPath).toEqual(path.join(dir, 'backstop_data', 'json_report', 'jsonReport.json'));
+    expect(config.ciReportPath).toEqual(path.join(dir, 'backstop_data', 'ci_report', 'xunit.xml'));
   });
 
-  test('it reads html_report', () => {
+  test('it reads paths', () => {
     const dir = path.join(process.cwd(), 'test', 'fixtures', 'backstop', 'custom_report_path');
     const config = new Config(dir, 'backstop.json');
     expect(config.htmlReportPath).toEqual(path.join(dir, 'custom_backstop_data', 'html_report', 'config.js'));
+    expect(config.jsonReportPath).toEqual(path.join(dir, 'custom_backstop_data', 'json_report', 'jsonReport.json'));
+    expect(config.ciReportPath).toEqual(path.join(dir, 'custom_backstop_data', 'ci_report', 'xunit.xml'));
   });
 });
