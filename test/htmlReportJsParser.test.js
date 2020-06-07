6cc5600f204da163a4013248ea69558a859f49f4
hitode909 <hitode909@gmail.com>
2020-06-07 09:28:19 +0900

    rewrite

--- test/htmlReportJsParser.test.js
+++ test/htmlReportJsParser.test.js
@@ -1,15 +1,16 @@
-const os = require('os');
 const fs = require('fs');
-const path = require('path');
 const { extractConfigJs, writeConfigJs } = require('../lib/htmlReportJsParser');
+const {copy, resolve} = require('test-fixture')();
 
 describe('extractConfigJs', () => {
-  test('it throws error when the target file is missing or broken', () => {
-    expect(() => { extractConfigJs('test/fixtures/notExistFile') }).toThrow(/no such file or directory/);
-    expect(() => { extractConfigJs('test/fixtures/config.js.invalid') }).toThrow(/Unexpected token/);
+  test('it throws error when the target file is missing or broken', async () => {
+    await copy();
+    expect(() => { extractConfigJs(resolve('notExistFile')) }).toThrow(/no such file or directory/);
+    expect(() => { extractConfigJs(resolve('config.js.invalid')) }).toThrow(/Unexpected token/);
   });
-  test('it extracts argument from JavaScript string', () => {
-    expect(extractConfigJs('test/fixtures/config.js.valid')).toStrictEqual({
+  test('it extracts argument from JavaScript string file', async () => {
+    await copy();
+    expect(extractConfigJs(resolve('config.js.valid'))).toStrictEqual({
       testSuite: 'BackstopJS',
       id: 'backstop_default',
       tests: [
@@ -25,8 +26,9 @@ describe('extractConfigJs', () => {
 });
 
 describe('writeConfigJs', () => {
-  test('it writes config.js to specified path', () => {
-    const tmpPath = path.join(os.tmpdir(), Math.random().toString());
+  test('it writes config.js to specified path', async () => {
+    await copy();
+    const tmpPath = resolve('tmp.json');
     writeConfigJs(tmpPath, { tests: [] });
     expect(fs.readFileSync(tmpPath).toString()).toEqual(`report({\n  "tests": []\n});`);
   });
