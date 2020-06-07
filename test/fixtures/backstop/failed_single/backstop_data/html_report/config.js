b877cde4ac1230cb5865d325761da092e6fbefd1
hitode909 <hitode909@gmail.com>
2020-06-07 12:35:48 +0900

    more test

---
+++ test/fixtures/backstop/failed_single/backstop_data/html_report/config.js
@@ -0,0 +1,32 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200606-175055/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://garris.github.io/BackstopJS/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.96",
+          "analysisTime": 30
+        },
+        "diffImage": "../bitmaps_test/20200606-175055/failed_diff_backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
